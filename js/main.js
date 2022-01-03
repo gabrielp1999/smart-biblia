ab55c75e7de5998138fd3ce0ab5a75aa3024ae73
Gabriel <gabriel.gsm2017@gmail.com>
2022-01-03 19:34:58 -0300

    add token

--- js/main.js
+++ js/main.js
@@ -14,7 +14,11 @@ function onClickBtnFiltros() {
 
 async function buscarVersiculoAleatorio() {
 
-    const resultado = await axios ("https://www.abibliadigital.com.br/api/verses/nvi/random");
+    const resultado = await axios.get("https://www.abibliadigital.com.br/api/verses/nvi/random", {
+        headers: {
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik1vbiBKYW4gMDMgMjAyMiAyMjoyMDo1MCBHTVQrMDAwMC5mbGlwZ2dzQGdtYWlsLmNvbSIsImlhdCI6MTY0MTI0ODQ1MH0.pUjeniyc77c0NC8z3uKsj0AmggL-i_voe10kVhDZu84"
+        }
+    });
 
     const versiculoAleatorio = document.getElementById('versiculoAleatorio');
     const livroAleatorio = document.getElementById('livroAleatorio');
@@ -27,7 +31,11 @@ async function buscarVersiculoAleatorio() {
 
 
 async function buscarVersoes() {
-    const resultado = await axios ("https://www.abibliadigital.com.br/api/versions");
+    const resultado = await axios.get("https://www.abibliadigital.com.br/api/versions", {
+        headers: {
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik1vbiBKYW4gMDMgMjAyMiAyMjoyMDo1MCBHTVQrMDAwMC5mbGlwZ2dzQGdtYWlsLmNvbSIsImlhdCI6MTY0MTI0ODQ1MH0.pUjeniyc77c0NC8z3uKsj0AmggL-i_voe10kVhDZu84"
+        }
+    });
 
     const ulVersoes = document.getElementById('ulVersoes');
 
@@ -41,8 +49,8 @@ async function buscarVersoes() {
 
 }
 
-buscarVersoes()
-buscarVersiculoAleatorio()
+buscarVersoes();
+buscarVersiculoAleatorio();
 
 
 
