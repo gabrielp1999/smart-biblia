5fff3a9234e8afc37e7f8a0d43423a1d5aad150d
Gabriel <gabriel.gsm2017@gmail.com>
2022-01-03 19:28:40 -0300

    ligando o js na Api

--- js/main.js
+++ js/main.js
@@ -14,19 +14,34 @@ function onClickBtnFiltros() {
 
 async function buscarVersiculoAleatorio() {
 
-    let resultado = await axios ("https://www.abibliadigital.com.br/api/verses/nvi/random");
+    const resultado = await axios ("https://www.abibliadigital.com.br/api/verses/nvi/random");
 
     const versiculoAleatorio = document.getElementById('versiculoAleatorio');
     const livroAleatorio = document.getElementById('livroAleatorio');
 
-    let conteudoLivro = `${resultado.data.book.name} ${resultado.data.chapter}.${resultado.data.number} `
+    let conteudoLivro = `${resultado.data.book.name} ${resultado.data.chapter}:${resultado.data.number} `
 
     livroAleatorio.innerHTML = conteudoLivro;
     versiculoAleatorio.innerHTML = resultado.data.text;
-
-    console.log(resultado.data)
 }
 
+
+async function buscarVersoes() {
+    const resultado = await axios ("https://www.abibliadigital.com.br/api/versions");
+
+    const ulVersoes = document.getElementById('ulVersoes');
+
+    let conteudoVersao = ``
+
+    for(let versao of resultado.data) {
+        conteudoVersao += `<li><button href="">${versao.version.toUpperCase()}</button></li>`
+    }
+
+    ulVersoes.innerHTML = conteudoVersao;
+
+}
+
+buscarVersoes()
 buscarVersiculoAleatorio()
 
 
